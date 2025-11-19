62a46598e5b74c92e935ed31a3fe5c2a38fee6c4
AlvaniHer <99564929+AlvaniHer@users.noreply.github.com>
2025-11-19 16:49:53 +0100

    Segundo commit

---
+++ Assets/Script/Botones.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botones:MonoBehaviour
+{
+
+    // Método para iniciar el juego
+    public void Reinicio()
+    {
+        // Carga la escena del juego 
+        SceneManager.LoadScene("Nivel1"); //Nivel principal 
+    }
+
+    
+    // Método para salir del juego
+    public void Salir()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+
+}
